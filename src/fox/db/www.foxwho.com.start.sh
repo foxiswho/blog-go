037eeaaf1dad6ae339d5c15b96cb9bc0445fca63
fox <foxiswho@gmail.com>
2017-01-05 19:01:45 +0800

    没有任何日志

--- src/fox/db/www.foxwho.com.start.sh
+++ src/fox/db/www.foxwho.com.start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-nohup /www/wwwroot/www.foxwho.com/fox & 
+nohup /www/wwwroot/www.foxwho.com/fox >/www/wwwroot/www.foxwho.com.log 2>&1 &
+
+#没有任何日志
+##nohup /www/wwwroot/www.foxwho.com/fox >/dev/null 2>&1 &
