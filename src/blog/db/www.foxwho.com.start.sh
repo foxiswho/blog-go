0ce05906e1192a170f8d03bca4a8d932df6586d3
fox <foxiswho@gmail.com>
2017-04-10 21:22:04 +0800

    修复模版路径错误

--- src/blog/db/www.foxwho.com.start.sh
+++ src/blog/db/www.foxwho.com.start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-nohup /www/wwwroot/www.foxwho.com/blog >/www/wwwroot/www.foxwho.com.log 2>&1 &
+cd /www/wwwroot/www.foxwho.com
+nohup ./blog >/www/wwwroot/www.foxwho.com.log 2>&1 &
 
 #没有任何日志
 ##nohup /www/wwwroot/www.foxwho.com/blog >/dev/null 2>&1 &
