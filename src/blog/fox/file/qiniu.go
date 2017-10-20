7d474b2cce472e637914ff676897effd1179ac43
fox <foxiswho@gmail.com>
2017-10-20 13:22:35 +0800

    七牛新版API

--- src/blog/fox/file/qiniu.go
+++ src/blog/fox/file/qiniu.go
@@ -1,35 +1,37 @@
 package file
 
 import (
-	"qiniupkg.com/api.v7/kodo"
-	"qiniupkg.com/api.v7/conf"
-	"qiniupkg.com/api.v7/kodocli"
-	"fmt"
 	"blog/fox"
-	"time"
 	"blog/fox/cache"
-	"mime/multipart"
-	"strings"
-	"os"
 	"blog/fox/config"
+	"context"
+	"fmt"
+	"github.com/qiniu/api.v7/auth/qbox"
+	"github.com/qiniu/api.v7/storage"
+	"mime/multipart"
+	"os"
+	"strings"
+	"time"
 )
+
 //七牛云存储
 type QiNiu struct {
 	Config map[string]string `json:"-"`
 }
-
 
 // 构造返回值字段
 type PutRet struct {
 	Hash string `json:"hash"`
 	Key  string `json:"key"`
 }
+
 //初始化
 func NewQiNiu() *QiNiu {
 	return new(QiNiu)
 }
+
 //获取配置
-func (t *QiNiu)setConfig() (bool, error) {
+func (t *QiNiu) setConfig() (bool, error) {
 	maps, err := config.GetSection("qiniu")
 	if err != nil {
 		return false, err
@@ -37,8 +39,9 @@ func (t *QiNiu)setConfig() (bool, error) {
 	t.Config = maps
 	return true, nil
 }
+
 //七牛配置读取
-func (t *QiNiu)SetQiNiuConfig() (bool, error) {
+func (t *QiNiu) SetQiNiuConfig() (bool, error) {
 	ok, err := t.setConfig()
 	if err != nil {
 		fmt.Println("setConfig err:", err)
@@ -46,36 +49,31 @@ func (t *QiNiu)SetQiNiuConfig() (bool, error) {
 	}
 	fmt.Println("setConfig", ok)
 	if len(t.Config) < 1 {
-		return false,fox.NewError("配置文件没有读取")
+		return false, fox.NewError("配置文件没有读取")
 	}
-	// 初始化AK，SK
-	conf.ACCESS_KEY = t.Config["access_key"]
-	conf.SECRET_KEY = t.Config["secret_key"]
 	return true, nil
 }
+
 //设置 token 缓存
-func (t *QiNiu)setToken() string {
-	// 设置上传到的空间
-	bucket := t.Config["bucket"]
-	// 创建一个Client
-	c := kodo.New(0, nil)
-	// 设置上传的策略
-	policy := &kodo.PutPolicy{
-		Scope:   bucket,
-		//设置Token过期时间
-		Expires: 3600,
+func (t *QiNiu) setToken() string {
+	// 初始化AK，SK
+	putPolicy := storage.PutPolicy{
+		Scope:   t.Config["bucket"], // 设置上传到的空间
+		Expires: 3600,               //设置Token过期时间
 	}
+	mac := qbox.NewMac(t.Config["access_key"], t.Config["secret_key"])
 	// 生成一个上传token
-	token := c.MakeUptoken(policy)
+	token := putPolicy.UploadToken(mac)
 	//缓存
-	err := cache.Put("qiniu_token", token, 3600 * time.Second)
+	err := cache.Put("qiniu_token", token, 3600*time.Second)
 	if err != nil {
 		fmt.Println("设置缓存错误", err)
 	}
 	return token
 }
+
 //获取
-func (t *QiNiu)GetToken() string {
+func (t *QiNiu) GetToken() string {
 	if !cache.IsExist("qiniu_token") {
 		return t.setToken()
 	}
@@ -86,26 +84,38 @@ func (t *QiNiu)GetToken() string {
 	}
 	return tmp
 }
+
 //上传
-func (t *QiNiu)Upload(file multipart.File, UploadFile *UploadFile) (interface{}, error) {
+func (t *QiNiu) Upload(file multipart.File, UploadFile *UploadFile) (interface{}, error) {
 	//七牛配置填充
 	_, err := t.SetQiNiuConfig()
 	if err != nil {
 		fmt.Println("getConfig err:", err)
-		return nil,fox.NewError("七牛配置错误:" + err.Error())
+		return nil, fox.NewError("七牛配置错误:" + err.Error())
 	}
 	//token
 	token := t.GetToken()
 	if len(token) < 1 {
 		fmt.Println("token", token)
-		return nil,fox.NewError("七牛token不能为空")
+		return nil, fox.NewError("七牛token不能为空")
 	}
-	// 构建一个uploader
-	uploader := kodocli.NewUploader(0, nil)
+	cfg := storage.Config{}
+	// 空间对应的机房
+	cfg.Zone = &storage.ZoneHuadong
+	// 是否使用https域名
+	cfg.UseHTTPS = false
+	// 上传是否使用CDN上传加速
+	cfg.UseCdnDomains = false
+	// 构建表单上传的对象
+	formUploader := storage.NewFormUploader(&cfg)
+	ret := storage.PutRet{}
+	// 可选配置
+	putExtra := storage.PutExtra{}
+
 	//当前的目录
 	dir, _ := os.Getwd()
 	fmt.Println("当前的目录", dir)
-	var ret PutRet
+
 	// 设置上传文件的路径
 	key := UploadFile.Path + UploadFile.Name
 	filePath := dir + UploadFile.GetLocalTmpPath() + UploadFile.Name
@@ -113,7 +123,7 @@ func (t *QiNiu)Upload(file multipart.File, UploadFile *UploadFile) (interface{},
 	fmt.Println("本地文件绝对路径", filePath)
 	fmt.Println("去除第一个字符/后，访问路径", key)
 	// 调用PutFileWithoutKey方式上传，没有设置saveasKey以文件的hash命名
-	err = uploader.PutFile(nil, &ret, token, key, filePath, nil)
+	err = formUploader.PutFile(context.Background(), &ret, token, key, filePath, &putExtra)
 	// 打印出错信息
 	if err != nil {
 		fmt.Println("io.Put failed:", err)
